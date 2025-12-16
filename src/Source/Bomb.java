90bb7b53baa7aad1132a31751095877a07c8f5ef
KJH198 <kjh15071687155@126.com>
2025-12-16 18:49:32 +0800

    飞机大战游戏1.0

--- src/Source/Bomb.java
+++ src/Source/Bomb.java
@@ -28,8 +28,8 @@ public class Bomb extends GameObject {
     }
 
     public void move(){
-        x += (int) speed * Math.cos(degree);
-        y += (int) speed * Math.sin(degree);
+        x += speed * (int)Math.cos(degree);
+        y += speed * (int)Math.sin(degree);
         if (x < 0){
             x = 0;
             degree = Math.PI - degree;
