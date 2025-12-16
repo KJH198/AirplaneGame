d2f64754f266fdc4f7ab9fa5167be6242661ad68
KJH198 <kjh15071687155@126.com>
2025-12-16 19:45:56 +0800

    飞机大战游戏2.0

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import Source.GameFrame;
 public class Main {
     public static void main(String[] args) {
         GameFrame frame = new GameFrame();
-        frame.launchGameFrame(10);
+        frame.launchGameFrame(15);
     }
 }
